6ee09f2c19b83f265a883d54f574f5ecbe5a25ae
Mark Natera <marknatera@gmail.com>
2017-03-29 22:48:30 -0400

    WIP

--- video-transfer.rb
+++ video-transfer.rb
@@ -1,20 +1,23 @@
-# HandBrakeCLI -i /Volumes/media/LIVE\ Recordings/2016-06-15_Pastor\ Joaquin\ 1\ \(PGM\).mpg -o ~/Desktop/testing-video/pastor.mp4 --preset="High Profile"
-# Preset List for conversontions
-# HandBrakeCLI --preset-list
 require 'rubygems'
 require 'awesome_print'
 require 'handbrake'
-# Input
-# video = HandBrake::CLI.new.input('/Users/Mark/Desktop/testing-video/video.mp4')
-videos = []
-folder = File.open(' ')
+require 'fileutils'
+
+# For Testing
+# videos = Dir["/Volumes/Media/LIVE\ Recordings/*"]
+# videos = Dir["*.mov"]
+
+# SOL Server Path
+videos = Dir["/mnt/SOLNAS/LIVE Recordings*"]
+
 videos.each do |video|
-  # require 'debug'
-  # video = HandBrake::CLI.new.input('/Users/Mark/Desktop/testing-video/video.mp4')
+  require 'debug'
+  file = File.basename(video,File.extname(video))
+  FileUtils.cp(video, "~/Videos/.")
+  video = "~/Videos/" + file + ".mov"
+  vconvert = HandBrake::CLI.new(trace: true).input(video)
+  vconvert.preset('High Profile').output("~/Videos/converted/#{file}.mp4")
 end
 # video = HandBrake::CLI.new.input('/Users/Mark/OneDriveBusiness/Playa/Images/1.mp4')
 # For iPhone
 # video.preset('iPhone & iPod Touch').output('/Users/Mark/Desktop/project-phone.m4v')
-
-# TV
-video.preset('High Profile').output('project-tv.mp4')
