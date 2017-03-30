3e0a67d6374a3ea439c548a775748c79ed868c2c
Mark Natera <marknatera@gmail.com>
2017-03-30 11:34:24 -0400

    WIP

--- video-transfer.rb
+++ video-transfer.rb
@@ -3,21 +3,19 @@ require 'awesome_print'
 require 'handbrake'
 require 'fileutils'
 
-# For Testing
+### For Testing
 # videos = Dir["/Volumes/Media/LIVE\ Recordings/*"]
 # videos = Dir["*.mov"]
-
-# SOL Server Path
-videos = Dir["/mnt/SOLNAS/LIVE Recordings*"]
+### SOL Server Path
+videos = Dir["/mnt/SOLNAS/LIVE\ Recordings/*.mov"]
 
 videos.each do |video|
-  require 'debug'
+  # require 'debug'
   file = File.basename(video,File.extname(video))
-  FileUtils.cp(video, "~/Videos/.")
-  video = "~/Videos/" + file + ".mov"
+  FileUtils.cp(video, ".")
+  video = file + ".mov"
   vconvert = HandBrake::CLI.new(trace: true).input(video)
-  vconvert.preset('High Profile').output("~/Videos/converted/#{file}.mp4")
+  vconvert.preset('High Profile').output("converted/#{file}.mp4")
+  # FileUtils.rm_rf(video, ".")
+  File.delete(video)
 end
-# video = HandBrake::CLI.new.input('/Users/Mark/OneDriveBusiness/Playa/Images/1.mp4')
-# For iPhone
-# video.preset('iPhone & iPod Touch').output('/Users/Mark/Desktop/project-phone.m4v')
