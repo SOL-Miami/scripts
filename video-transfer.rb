d5aaf1097f8e4b60ae0313bf879378f40695426c
Mark Natera <marknatera@gmail.com>
2017-04-02 12:34:17 -0400

    WIP

--- video-transfer.rb
+++ video-transfer.rb
@@ -4,18 +4,22 @@ require 'handbrake'
 require 'fileutils'
 
 ### For Testing
-# videos = Dir["/Volumes/Media/LIVE\ Recordings/*"]
+videos = Dir["/Volumes/Media/LIVE\ Recordings/*"]
 # videos = Dir["*.mov"]
 ### SOL Server Path
-videos = Dir["/mnt/SOLNAS/LIVE\ Recordings/*.mov"]
+# videos = Dir["/mnt/SOLNAS/LIVE\ Recordings/*.mov"]
 
 videos.each do |video|
+  file = File.basename(video,File.extname(video)) + ".mp4"
+  file_folder = "/mnt/SOLNAS/LIVE\ Recordings/converted/#{file}"
+  unless File.exist?(file_folder)
+    # require 'debug'
+    vconvert = HandBrake::CLI.new(trace: true).input(video)
+    vconvert.preset('High Profile').output("/mnt/SOLNAS/LIVE\ Recordings/converted/#{file}")
+  end
+  # FileUtils.cp(video, ".")
+  # video = file + ".mov"
   # require 'debug'
-  file = File.basename(video,File.extname(video))
-  FileUtils.cp(video, ".")
-  video = file + ".mov"
-  vconvert = HandBrake::CLI.new(trace: true).input(video)
-  vconvert.preset('High Profile').output("converted/#{file}.mp4")
   # FileUtils.rm_rf(video, ".")
-  File.delete(video)
+  # File.delete(video)
 end
